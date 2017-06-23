9372cdcd10a0f85b9661a37d590cfc75336634b7
Ray <ray@vanchu.net>
2017-06-23 17:25:33 +0800

    update

--- test/test.js
+++ test/test.js
@@ -2,28 +2,13 @@ const EchoClient = require('../examples/echo/client');
 const EchoServer = require('../examples/echo/server');
 const assert = require('assert');
 
-describe('#echo', function() {
+describe("#tcp-framework", function() {
 	it('should return [hello, world]', testEcho);
-});
-describe('#echoWithDelayedStartup', function() {
-	this.timeout(5000);
-	it('should return [hello, world]', testEchoWithDelayedStartup);
-});
+})
 
 async function testEcho() {
 	let server = new EchoServer();
 	server.start();
-
-	let client = new EchoClient();
-	let response = await client.send('hello, world');
-	assert(response === 'hello, world');
-}
-
-async function testEchoWithDelayedStartup() {
-	setTimeout(() => {
-		let server = new EchoServer();
-		server.start();
-	}, 2000);
 
 	let client = new EchoClient();
 	let response = await client.send('hello, world');
